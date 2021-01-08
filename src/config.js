cf3c35c25fd72aa12930063edc19ccc3fda829f4
Łukasz Śliwiński <lsliwinski@pgs-soft.com>
2021-01-08 22:33:58 +0100

    My blog

--- src/config.js
+++ src/config.js
@@ -1,24 +1,18 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "N2Y4NzhiOGJlYTQxZmY4MjYxNjYxNmRhY2VhMzFiMjg0Yzk2N2NjYg==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "nonameplum",
 
   // Your Github Repo Name Where You Have your issues as Blog
-  githubRepo: "react-blog-github",
-
-  // Set it to true if you have a Github app to add to this project
-  // and fill the client ID & secret
-  enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  githubRepo: "blog",
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "Plum's blog" ,
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle : "iOS Developer blog",
 
   // Header customization
   header: {
