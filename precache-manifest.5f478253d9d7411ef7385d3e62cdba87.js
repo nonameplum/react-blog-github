e93b928f391b197469d0ab61d7e97915caa9424e
Łukasz Śliwiński <lsliwinski@pgs-soft.com>
2021-01-08 22:49:13 +0100

    Updates

---
+++ precache-manifest.5f478253d9d7411ef7385d3e62cdba87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203316a33f6910058e096976828985da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "571c5bead3d38369a98e",
+    "url": "/static/css/main.87c6aefc.chunk.css"
+  },
+  {
+    "revision": "97fc09a637234e5c103e",
+    "url": "/static/js/2.a2af18b5.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/static/js/2.a2af18b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "571c5bead3d38369a98e",
+    "url": "/static/js/main.290e0070.chunk.js"
+  },
+  {
+    "revision": "f34df4cc19e0da90cba3",
+    "url": "/static/js/runtime-main.578b6095.js"
+  }
+]);
